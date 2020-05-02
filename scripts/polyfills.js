21194816020b7e73cf73d1d6ace37ca7de3a68fd
HOKEN\hoken <omartin@tresc.cat>
2020-05-02 13:06:30 +0200

    Trying to get pages to work

--- scripts/polyfills.js
+++ scripts/polyfills.js
@@ -1,4 +1,3 @@
-// https://tc39.github.io/ecma262/#sec-array.prototype.find
 if (!Array.prototype.find) {
     Object.defineProperty(Array.prototype, 'find', {
         value: function (predicate) {
