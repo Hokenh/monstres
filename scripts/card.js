7dde3eb6c6c546b8e62a751de1ff6da9e2fdb8db
HOKEN\hoken <omartin@tresc.cat>
2020-05-02 12:29:51 +0200

    Added analytics

--- scripts/card.js
+++ scripts/card.js
@@ -16,16 +16,16 @@ Vue.component("monsterCard", {
   computed: {
     cardClasses: function () {
       var c = {};
-      c['monstercard--face-down'] = this.faceDown;
+      c["monstercard--face-down"] = this.faceDown;
       c[this.deck] = true;
       return c;
     },
     cardFigure: function () {
       return {
-        "background": "url(" + this.image + ") no-repeat center",
-        "background-size": "contain"
-      }
-    }
+        background: "url(" + this.image + ") no-repeat center",
+        "background-size": "contain",
+      };
+    },
   },
   methods: {
     flip: function () {
@@ -38,18 +38,19 @@ Vue.component("monsterCard", {
       this.faceDown = false;
     },
   },
-  template: "<div class='monstercard-container'>" +
-    "  <h2 class=\"deck-title\" @click=\"$emit('request-new-card')\" v-bind:alt=\"newcardliteral\">{{deckname}}</h2>" +
-    "  <div class=\"monstercard\" @click=\"flip\" :class=\"cardClasses\"  >" +
+  template:
+    "<div class='monstercard-container'>" +
+    '  <h2 class="deck-title" @click="$emit(\'request-new-card\')" v-bind:alt="newcardliteral">{{deckname}}</h2>' +
+    '  <div class="monstercard" @click="flip" :class="cardClasses"  >' +
     '     <div class="front">' +
     '       <span class="power">{{power}}</span>' +
     '         <div class="inner-grid">' +
     '            <div><img :src="image" draggable="false" :alt="deckname" :title="deckname"></div>' +
     '            <div class="syllabe">{{syllabe}}</div>' +
-    '         </div>' +
+    "         </div>" +
     "     </div>" +
     '     <div class="back"></div>' +
     "   </div>" +
     ' <div style="text-align:center; margin-top:2em"><button v-show="!faceDown" class="button is-primary" @click="$emit(\'request-new-card\')" v-bind:alt="newcardliteral" v-bind:title="newcardliteral"><img style="margin: 5px; width: 1rem;stroke-color:white;" src="assets/icons/refresh.svg"/></button></div>' +
-    "</div >"
+    "</div >",
 });
